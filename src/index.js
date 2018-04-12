6c30bb4b8030a1d06673d736369de5e724a98309
metalex9 <alexbainter@gmail.com>
2018-04-11 20:58:23 -0500

    Tweak performance a bit.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import Tone from "tone";
 import addSinger from "./add-singer";
 
+Tone.context.latencyHint = "playback";
+
 const singers = [];
 
-const numSingers = 5;
+const numSingers = 10;
 
 for (let i = 1; i <= numSingers; i++) {
   const singer = addSinger(singers);
   singer.schedulePiece();
 }
 
-Tone.Transport.start();
+Tone.Transport.start("+0.5");
